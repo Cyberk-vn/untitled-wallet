6f5a0715bb022194829382c975e1d1a2fdc526cb
Treacle <68235687+bangbu10820@users.noreply.github.com>
2025-01-24 15:40:25 +0700

    draft scene

--- apps/extension/src/pages/register/import-wallet/import-passphrase.tsx
+++ apps/extension/src/pages/register/import-wallet/import-passphrase.tsx
@@ -1,11 +1,20 @@
 import React, { FunctionComponent } from "react";
-import { Box } from "../../../components/box";
 import { RegisterSimpleHeaderWithSeparator } from "../components/simple-header-with-separator";
+import { RegisterSceneBoxBody } from "../components/register-scene-box-with-height";
+import { Button } from "../../../components/button";
+import { RegisterSceneBoxFooter } from "../components/register-scene-box-with-height";
+import { RegisterSceneBoxWithHeight } from "../components/register-scene-box-with-height";
 
 export const RegisterImportPassphraseScene: FunctionComponent = () => {
   return (
-    <Box>
-      <RegisterSimpleHeaderWithSeparator title="Import from passphrase" />
-    </Box>
+    <RegisterSceneBoxWithHeight title="Import from passphrase">
+      <RegisterSceneBoxBody>
+        <RegisterSimpleHeaderWithSeparator title="Import from passphrase" />
+      </RegisterSceneBoxBody>
+      <RegisterSceneBoxFooter>
+        <Button mode="outline" text="Back" />
+        <Button text="Next" />
+      </RegisterSceneBoxFooter>
+    </RegisterSceneBoxWithHeight>
   );
 };
--- apps/extension/src/pages/register/import-wallet/import-private-key.tsx
+++ apps/extension/src/pages/register/import-wallet/import-private-key.tsx
@@ -1,11 +1,21 @@
 import React, { FunctionComponent } from "react";
-import { Box } from "../../../components/box";
 import { RegisterSimpleHeaderWithSeparator } from "../components/simple-header-with-separator";
+import {
+  RegisterSceneBoxBody,
+  RegisterSceneBoxFooter,
+  RegisterSceneBoxWithHeight,
+} from "../components/register-scene-box-with-height";
+import { Button } from "../../../components/button";
 
 export const RegisterImportPrivateKeyScene: FunctionComponent = () => {
   return (
-    <Box>
-      <RegisterSimpleHeaderWithSeparator title="Import from private key" />
-    </Box>
+    <RegisterSceneBoxWithHeight title="Import from private key">
+      <RegisterSceneBoxBody>
+        <RegisterSimpleHeaderWithSeparator title="Import from private key" />
+      </RegisterSceneBoxBody>
+      <RegisterSceneBoxFooter>
+        <Button mode="outline" text="Back" />
+      </RegisterSceneBoxFooter>
+    </RegisterSceneBoxWithHeight>
   );
 };
