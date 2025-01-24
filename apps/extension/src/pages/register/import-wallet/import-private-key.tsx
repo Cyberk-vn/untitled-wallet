8f399c29a73b03252c1baf1dd571c9e6031a6a33
Treacle <68235687+bangbu10820@users.noreply.github.com>
2025-01-24 16:19:08 +0700

    draft UI import private key scene

--- apps/extension/src/pages/register/import-wallet/import-private-key.tsx
+++ apps/extension/src/pages/register/import-wallet/import-private-key.tsx
@@ -6,24 +6,52 @@ import {
 } from "../components/register-scene-box-with-height";
 import { Button } from "../../../components/button";
 import { Column, Columns } from "../../../components/column";
-import { H1 } from "../../../components/typography";
+import { H4, Subtitle3 } from "../../../components/typography";
+import { Gutter } from "../../../components/gutter";
+import { TextInput } from "../../../components/input";
+import { useSceneTransition } from "../../../components/transition";
 
 export const RegisterImportPrivateKeyScene: FunctionComponent = () => {
+  const sceneTransition = useSceneTransition();
+
   return (
-    <RegisterSceneBoxWithHeight title="Import from private key">
-      <RegisterSceneBoxBody>
-        <H1>Import from private key - Body</H1>
-      </RegisterSceneBoxBody>
-      <RegisterSceneBoxFooter>
-        <Columns sum={2} gutter="0.75rem">
-          <Column weight={1}>
-            <Button mode="outline" text="Back" />
-          </Column>
-          <Column weight={1}>
-            <Button text="Next" disabled />
-          </Column>
-        </Columns>
-      </RegisterSceneBoxFooter>
-    </RegisterSceneBoxWithHeight>
+    <form>
+      <RegisterSceneBoxWithHeight title="Import from private key">
+        <RegisterSceneBoxBody>
+          <H4>Enter private key</H4>
+          <Gutter size="1.5rem" />
+          <Subtitle3
+            style={{
+              textAlign: "start",
+            }}
+          >
+            Private Key
+          </Subtitle3>
+          <Gutter size="0.375rem" />
+          <TextInput
+            placeholder="Enter private key"
+            style={{
+              borderRadius: "1rem",
+            }}
+          />
+        </RegisterSceneBoxBody>
+        <RegisterSceneBoxFooter>
+          <Columns sum={2} gutter="0.75rem">
+            <Column weight={1}>
+              <Button
+                mode="outline"
+                text="Back"
+                onClick={() => {
+                  sceneTransition.pop();
+                }}
+              />
+            </Column>
+            <Column weight={1}>
+              <Button text="Next" disabled type="submit" />
+            </Column>
+          </Columns>
+        </RegisterSceneBoxFooter>
+      </RegisterSceneBoxWithHeight>
+    </form>
   );
 };
