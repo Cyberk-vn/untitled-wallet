a8b6e7ead5e7658d473a16ec67eba49458e5cf77
Treacle <68235687+bangbu10820@users.noreply.github.com>
2025-01-29 15:48:08 +0700

    feat: Implement UI for 12-word recovery phrase input

--- apps/extension/src/pages/register/import-wallet/import-passphrase.tsx
+++ apps/extension/src/pages/register/import-wallet/import-passphrase.tsx
@@ -4,13 +4,42 @@ import { Button } from "../../../components/button";
 import { RegisterSceneBoxFooter } from "../components/register-scene-box-with-height";
 import { RegisterSceneBoxWithHeight } from "../components/register-scene-box-with-height";
 import { Column, Columns } from "../../../components/column";
-import { H1 } from "../../../components/typography";
+import { H4 } from "../../../components/typography";
+import { Gutter } from "../../../components/gutter";
+import { Box } from "../../../components/box";
+
+import styled from "styled-components";
+
+export const Styles = {
+  WordsGridContainer: styled.div<{
+    columns: number;
+  }>`
+    display: grid;
+    grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
+    gap: 0.75rem 0.75rem;
+  `,
+};
 
 export const RegisterImportPassphraseScene: FunctionComponent = () => {
   return (
     <RegisterSceneBoxWithHeight title="Import from passphrase">
       <RegisterSceneBoxBody>
-        <H1>Import from passphrase - Body</H1>
+        <Box alignX="center" paddingX="3rem">
+          <H4>Enter your 12 - word recovery phrase</H4>
+        </Box>
+        <Gutter size="1.25rem" />
+        <Styles.WordsGridContainer columns={2}>
+          {Array.from({ length: 12 }, (_, index) => (
+            <Box
+              key={index}
+              minHeight="2.625rem"
+              backgroundColor="#9898981A"
+              borderRadius="0.5rem"
+              borderWidth="1px"
+              borderColor="#9C9C9C1A"
+            />
+          ))}
+        </Styles.WordsGridContainer>
       </RegisterSceneBoxBody>
       <RegisterSceneBoxFooter>
         <Columns sum={2} gutter="0.75rem">
