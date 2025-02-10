d516726231aed260aadcfae6342d844223c4df87
AnyaCyberk <147572682+AnyaCyberk@users.noreply.github.com>
2025-02-10 09:41:30 +0700

    remove all mobile related (#6)
    Co-authored-by: Treacle <68235687+bangbu10820@users.noreply.github.com>

--- apps/mobile/init.ts
+++
@@ -1,7 +0,0 @@
-import './src/background/background';
-
-import {Titan} from '@titan-wallet/provider';
-import {RNMessageRequesterInternal} from './src/router';
-
-// @ts-ignore
-window.titan = new Titan('', 'core', new RNMessageRequesterInternal());
